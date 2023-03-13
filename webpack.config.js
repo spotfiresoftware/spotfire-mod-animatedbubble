01840abed07c7eadce7b1486bd07caa6c87d0591
Arnaud Varin <avarin@tibco.com>
2023-03-13 18:27:51 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+
+module.exports = {
+    mode: "development",
+    entry: "./src/main.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    plugins: [
+        new CopyPlugin({ patterns: [{ from: "static" }] })
+    ],
+    devtool: process.argv.includes("production") ? undefined : "inline-source-map"
+};
